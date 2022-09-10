99c3924b4d1766695c222b9cbdb080346785444b
shubhamojha1 <85477752+shubhamojha1@users.noreply.github.com>
2022-09-11 02:19:59 +0530

    Update A-Colored-Balls:Revisited.java

--- Codeforces/A-Colored-Balls:Revisited.java
+++ Codeforces/A-Colored-Balls:Revisited.java
@@ -1,22 +1,25 @@
-    import java.util.*;
-    public class Solution{
-        public static void main(String args[]){
-     
-            Scanner sc = new Scanner(System.in);
-            int t = sc.nextInt();
-            while(t--!=0){
-                int n = sc.nextInt();
-                int max = 0, pos=-1;
-                for(int i=0;i<n;i++){
-                    int cnt = sc.nextInt();
-                    if(cnt>max)
-                    {
-                        max=cnt;
-                        pos=i+1;
-                    }
-                }
-                System.out.println(pos);
-            }
-            sc.close();
+/*
+        https://codeforces.com/contest/1728/problem/A
+*/
+import java.util.*;
+public class Solution{
+public static void main(String args[]){
+
+Scanner sc = new Scanner(System.in);
+int t = sc.nextInt();
+while(t--!=0){
+    int n = sc.nextInt();
+    int max = 0, pos=-1;
+    for(int i=0;i<n;i++){
+        int cnt = sc.nextInt();
+        if(cnt>max)
+        {
+            max=cnt;
+            pos=i+1;
         }
     }
+    System.out.println(pos);
+}
+sc.close();
+}
+}
