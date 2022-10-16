bf686bfc4368db612c5ac4114fdd487327100e63
shubhamojha1 <85477752+shubhamojha1@users.noreply.github.com>
2022-10-16 10:27:43 +0530

    Create 1741A-Compare-TShirt-Sizes.cpp

---
+++ Codeforces/1741A-Compare-TShirt-Sizes.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define forn(i,n) for (int i=0;i<int(n);i++)
+#define size(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define eb emplace_back
