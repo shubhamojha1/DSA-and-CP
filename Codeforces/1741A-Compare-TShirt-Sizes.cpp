6cf227b7bfb94ffac9711ad61e1c0d95fa5563f7
shubhamojha1 <85477752+shubhamojha1@users.noreply.github.com>
2022-10-16 10:59:01 +0530

    Update 1741A-Compare-TShirt-Sizes.cpp

--- Codeforces/1741A-Compare-TShirt-Sizes.cpp
+++ Codeforces/1741A-Compare-TShirt-Sizes.cpp
@@ -5,3 +5,29 @@ using namespace std;
 #define size(v) (int)v.size()
 #define all(v) v.begin(), v.end()
 #define eb emplace_back
+
+void solve(){
+    string a,b; cin>>a>>b;
+    char ca = a.back();
+    char cb = b.back();
+
+    if(ca==cb){
+        if(size(a)==size(b)) cout<<"=";
+        else if(ca=='S')
+        cout << (size(a)<size(b) ? '>' : '<');
+        else
+        cout<< (size(a) < size(b) ? '<' : '>');
+    }
+    else cout << (ca < cb ? '>' : '<');
+    cout << '\n';
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    forn(i,t){
+        solve();
+    }
+}
+  
