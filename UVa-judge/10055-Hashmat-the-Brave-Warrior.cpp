09ed86fc65c013669def0cb1e7ca5342b0481683
shubhamojha1 <85477752+shubhamojha1@users.noreply.github.com>
2023-01-06 15:22:52 +0530

    keep in mind the manner of handling inputs

---
+++ UVa-judge/10055-Hashmat-the-Brave-Warrior.cpp
@@ -0,0 +1,36 @@
+// #include <bits/stdc++.h>
+#include <cmath>
+#include <iostream>
+#include <unordered_set>
+#define ll long long
+#define ld long double
+#define endl "\n"
+using namespace std;
+ll MOD = 1000000000 + 7;
+
+// void solve(int n){
+void solve(){
+    ll x, y;
+    while(cin>>x>>y){
+        cout<< abs(y-x)<< endl;
+    }
+    // if using cin>>x>>y --> comment out ios_base and cin.tie, cout.tie lines
+    // else, use while(scanf("%lld %lld", &x, &y) !=EOF)
+}
+  
+int main(){
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    // cout.tie(NULL);
+    solve();
+    // cout<<solve()<<endl;
+
+    /*
+            ******FOR TESTING*****
+    */
+    // int n;
+    // while(cin>>n){
+    //     // cout<<solve(n)<<endl;
+    //     solve(n);
+    // }
+}
